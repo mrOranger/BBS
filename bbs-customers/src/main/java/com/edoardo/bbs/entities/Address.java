d2b50b54b4a4e5db13e9709b50e1dd1a15334c52
Edoardo Oranger <edo.oranger@gmail.com>
2023-12-17 21:35:55 +0100

    Created Entity classes.

---
+++ bbs-customers/src/main/java/com/edoardo/bbs/entities/Address.java
@@ -0,0 +1,51 @@
+package com.edoardo.bbs.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity @Table(name = "Addresses") @Data
+public class Address implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2L;
+
+    @Id @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "country", nullable = false, length = 30)
+    private String country;
+
+    @Column(name = "state", nullable = false, length = 30)
+    private String state;
+
+    @Column(name = "city", nullable = false, length = 30)
+    private String city;
+
+    @Column(name = "street", nullable = false, length = 30)
+    private String street;
+
+    @Column(name = "number", nullable = false)
+    private Integer streetNumber;
+
+    @Column(name = "postal_code", nullable = false, length = 20)
+    private String postalCode;
+
+    @ManyToOne @EqualsAndHashCode.Exclude @JoinColumn(name = "tax_code", referencedColumnName = "taxCode")
+    @JsonManagedReference
+    private Customer customer;
+
+    public Address (Integer id, String country, String state, String city,
+                    String street, Integer streetNumber, String postalCode) {
+        this.id = id;
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.postalCode = postalCode;
+    }
+}
---
+++ bbs-customers/src/main/java/com/edoardo/bbs/entities/Customer.java
@@ -0,0 +1,60 @@
+package com.edoardo.bbs.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity @Table(name = "Customers") @Data
+public class Customer implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id @Column(name = "tax_code", length = 30)
+    private String taxCode;
+
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+
+    @Column(name = "birth_date", nullable = false)
+    private Date birthDate;
+
+    @Column(name = "email", nullable = false, unique = true, length = 20)
+    private String email;
+
+    @Column(name = "email_verified_at")
+    private Date emailVerifiedAt;
+
+    @Column(name = "password", nullable = false, length = 50)
+    private String password;
+
+    @Column(name = "id_card", nullable = false)
+    private String idCard;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade =  CascadeType.ALL, mappedBy = "customer", orphanRemoval = true)
+    @JsonManagedReference
+    private Set<Address> addresses;
+
+    public Customer(String taxCode, String firstName, String lastName, Date birthDate,
+                    String email, Date emailVerifiedAt, String password, String idCard) {
+        this.taxCode = taxCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.emailVerifiedAt = emailVerifiedAt;
+        this.password = password;
+        this.idCard = idCard;
+        this.addresses = new HashSet<>();
+    }
+}
