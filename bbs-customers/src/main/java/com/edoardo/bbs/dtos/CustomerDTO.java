92d564d087dada7625bc6ddec09815e8db83e937
Edoardo Oranger <edo.oranger@gmail.com>
2023-12-28 22:19:39 +0100

    Add first validation rules.

--- bbs-customers/src/main/java/com/edoardo/bbs/dtos/CustomerDTO.java
+++ bbs-customers/src/main/java/com/edoardo/bbs/dtos/CustomerDTO.java
@@ -1,5 +1,6 @@
 package com.edoardo.bbs.dtos;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,13 +10,23 @@ import java.util.Set;
 @Data @Builder
 public class CustomerDTO {
     private String taxCode;
+
+    @NotEmpty(message = "Customer first name must not be empty.")
     private String firstName;
+
+    @NotEmpty(message = "Customer last name must not be empty.")
     private String lastName;
+
     private LocalDate birthDate;
 
+    @NotEmpty(message = "Customer email must not be empty.")
     private String email;
     private LocalDate emailVerifiedAt;
+
+    @NotEmpty(message = "Customer password must not be empty.")
     private String password;
+
+    @NotEmpty(message = "Customer id card must not be empty.")
     private String idCard;
 
     private Set<AddressDTO> addresses;
