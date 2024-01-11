caa9ba536e9ca3115c83137f164f707c52902b7c
Edoardo Oranger <edo.oranger@gmail.com>
2024-01-11 21:39:26 +0100

    Implemented method to add an Address to existing customer.

--- bbs-customers/src/main/java/com/edoardo/bbs/services/implementation/AddressServiceImpl.java
+++ bbs-customers/src/main/java/com/edoardo/bbs/services/implementation/AddressServiceImpl.java
@@ -1,20 +1,28 @@
 package com.edoardo.bbs.services.implementation;
 
 import com.edoardo.bbs.dtos.AddressDTO;
+import com.edoardo.bbs.entities.Address;
+import com.edoardo.bbs.entities.Customer;
 import com.edoardo.bbs.exceptions.MaximumAddressNumberException;
 import com.edoardo.bbs.exceptions.ResourceNotFoundException;
 import com.edoardo.bbs.mapper.AddressMapper;
 import com.edoardo.bbs.repositories.AddressRepository;
+import com.edoardo.bbs.repositories.CustomerRepository;
 import com.edoardo.bbs.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Optional;
+import java.util.Set;
+
 public class AddressServiceImpl implements AddressService {
 
+    private final CustomerRepository customerRepository;
     private final AddressRepository addressRepository;
     private final AddressMapper addressMapper;
 
     @Autowired
-    public AddressServiceImpl(AddressRepository addressRepository, AddressMapper addressMapper) {
+    public AddressServiceImpl(AddressRepository addressRepository, CustomerRepository customerRepository, AddressMapper addressMapper) {
+        this.customerRepository = customerRepository;
         this.addressRepository = addressRepository;
         this.addressMapper = addressMapper;
     }
@@ -22,7 +30,17 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public AddressDTO addAddress(String taxCode, AddressDTO address) throws MaximumAddressNumberException, ResourceNotFoundException {
-        return null;
+        final Optional<Customer> customer = this.customerRepository.findById(taxCode);
+        if (customer.isPresent()) {
+            final Customer customerEntity = customer.get();
+            if (customerEntity.getAddresses().size() < 2) {
+                Address addressEntity = this.addressMapper.convertToEntity(address);
+                addressEntity.setCustomer(customerEntity);
+                return this.addressMapper.convertToDTO(this.addressRepository.save(addressEntity));
+            }
+            throw new MaximumAddressNumberException("Customer " + taxCode + ", maximum addresses reached.");
+        }
+        throw new ResourceNotFoundException("Customer " + taxCode + " not found.");
     }
 
     @Override
