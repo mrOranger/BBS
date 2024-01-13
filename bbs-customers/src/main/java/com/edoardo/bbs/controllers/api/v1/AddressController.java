f4e0e9922e41c9b894ac3fe2ae85cd1674fc1f67
Edoardo Oranger <edo.oranger@gmail.com>
2024-01-13 16:31:45 +0100

    Created Presentation layer for Address.

---
+++ bbs-customers/src/main/java/com/edoardo/bbs/controllers/api/v1/AddressController.java
@@ -0,0 +1,30 @@
+package com.edoardo.bbs.controllers.api.v1;
+
+
+import com.edoardo.bbs.dtos.AddressDTO;
+import com.edoardo.bbs.services.AddressService;
+import jakarta.validation.Valid;
+import lombok.SneakyThrows;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1") @Log
+public class AddressController {
+    public AddressService addressService;
+
+    @Autowired
+    public AddressController (AddressService addressService) {
+        this.addressService = addressService;
+    }
+
+    @PatchMapping(value = "/addresses/customer/{customer}", produces = "application/json") @SneakyThrows
+    public ResponseEntity<AddressDTO> addAddressToCustomer (
+            @PathVariable String customer,
+            @Valid @RequestBody AddressDTO address
+    ) {
+        return ResponseEntity.ok(this.addressService.addAddress(customer, address));
+    }
+}
