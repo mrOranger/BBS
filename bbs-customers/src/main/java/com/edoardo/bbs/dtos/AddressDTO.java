b3f27513cab8653bfd7f89e1c5e72e1a13a3956c
Edoardo Oranger <edo.oranger@gmail.com>
2024-01-13 15:24:07 +0100

    Add validation rules for Address DTO.

--- bbs-customers/src/main/java/com/edoardo/bbs/dtos/AddressDTO.java
+++ bbs-customers/src/main/java/com/edoardo/bbs/dtos/AddressDTO.java
@@ -1,16 +1,37 @@
 package com.edoardo.bbs.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
 @Data @Builder
 public class AddressDTO {
     private Integer id;
+
+    @NotNull(message = "Address country must be not empty.")
+    @Size(min = 1, max = 100, message = "Address country must be at most 100 character long.")
     private String country;
+
+    @NotNull(message = "Address state must be not empty.")
+    @Size(min = 1, max = 100, message = "Address state must be at most 100 character long.")
     private String state;
+
+    @NotNull(message = "Address city must be not empty.")
+    @Size(min = 1, max = 100, message = "Address city must be at most 100 character long.")
     private String city;
+
+    @NotNull(message = "Address city must be not empty.")
+    @Size(min = 1, max = 100, message = "Address city must be at most 100 character long.")
     private String street;
+
+    @NotNull(message = "Address street number must be not empty.")
+    @Min(value = 1)
     private Integer streetNumber;
+
+    @NotNull(message = "Address postal code must be not empty.")
+    @Size(min = 1, max = 50, message = "Address postal code must be at most 50 character long.")
     private String postalCode;
 
     private CustomerDTO customer;
