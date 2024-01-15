22fcd6732bc396f032a09a02ac3abcf63e61f13e
Edoardo Oranger <edo.oranger@gmail.com>
2024-01-15 22:08:27 +0100

    Implemented configuration file for Factories.

--- bbs-customers/src/main/java/com/edoardo/bbs/FactoryConfig.java
+++ bbs-customers/src/main/java/com/edoardo/bbs/FactoryConfig.java
@@ -4,17 +4,18 @@ package com.edoardo.bbs;
 import com.edoardo.bbs.factories.AddressFactory;
 import com.edoardo.bbs.factories.CustomerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FactoryConfig {
 
-    @Bean
+    @Bean @Scope("singleton")
     public AddressFactory addressFactory () {
         return new AddressFactory();
     }
 
-    @Bean
+    @Bean @Scope("singleton")
     public CustomerFactory customerFactory () {
         return new CustomerFactory();
     }
