c35bff9ce76233c4ac53a4df7c341fb1c78bbac8
Edoardo Oranger <edo.oranger@gmail.com>
2024-01-20 18:38:38 +0100

    Updated cache configuration.

--- bbs-customers/src/main/java/com/edoardo/bbs/BusinessBankSystemApplication.java
+++ bbs-customers/src/main/java/com/edoardo/bbs/BusinessBankSystemApplication.java
@@ -1,11 +1,7 @@
 package com.edoardo.bbs;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.function.Function;
 
 @SpringBootApplication
 public class BusinessBankSystemApplication {
